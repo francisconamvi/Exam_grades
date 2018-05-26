f4911c97d5cf2fe1beeb9e0570e5ea55d6dd7726
francisconavy <francisconavy@yahoo.com.br>
2018-05-26 16:25:10 -0300

    comecando a escrever no arquivo

--- main.py
+++ main.py
@@ -10,8 +10,7 @@ def Test():
     try:
         teste = open(name+".txt", "r")
         #if there isn't an error, it means that there is a file with the same name 
-        print("There is a profile with this name. Do you want to overwrite it? (y/n)")
-        des = input()
+        des = input("There is a profile with this name. Do you want to overwrite it? (y/n) ")
         if des == "y":
             pass
         if des == "n":
@@ -27,21 +26,25 @@ def New():
     #Registering the subjects
     enter = "y"
     while enter != "n":
-        subjects.append(input("Write the subject code: "))
-        #new_file.write(subjects[-1]+"/n")
+        subjects[input("Write the subject code: ")] = []
         enter = input("Want to register another subject?(y/n) ")
     print(subjects)
+    
+    #Add grades to each subject
+    print("Put the grade, or 'c' if you want to calculate it")
+    for subject in subjects:
+        for ps in range(1,4):
+            subjects[subject].append(input("%s P%d grade: "%(subject,ps)))
+    print(subjects)
+    
+    #Putting it into a file
+    for i in range(len(subjects)):
+        new_file.write(subjects[i])
 
 #MAIN
-subjects = list()
+subjects = dict()
 menu = input("New/Load: ")
 name = input("What's your name and last name? ")
 if menu == "New":
     Test()
     New()
-
-'''
-path = '/home/francisconavy/Projetos/notas_comp/workfile.txt'
-arq = open(path,'r')
-print(arq.read())
-'''
